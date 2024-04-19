987982171d0c3f8a8b68cd28c658f4791ef9ae94
softupcl <81437285+softupcl@users.noreply.github.com>
2024-04-19 11:40:14 -0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductosModule } from './productos/productos.module';
 import { OrdenesModule } from './ordenes/ordenes.module';
 import { NatsModule } from './transports/nats.module';
-import { AuthModule } from './auth/auth.module';
 import { HealthCheckModule } from './health-check/health-check.module';
 
 
 @Module({
-  imports: [ProductosModule, OrdenesModule, NatsModule, AuthModule,HealthCheckModule],
+  imports: [ProductosModule, OrdenesModule, NatsModule, HealthCheckModule],
   controllers: [],
   providers: [],
 })
