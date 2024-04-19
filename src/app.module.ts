10e15f31ffe3df74531445f60c2a4dbc6bb62dba
softupcl <81437285+softupcl@users.noreply.github.com>
2024-04-18 21:14:39 -0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { ProductosModule } from './productos/productos.module';
 import { OrdenesModule } from './ordenes/ordenes.module';
 import { NatsModule } from './transports/nats.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthCheckModule } from './health-check/health-check.module';
 
 
 @Module({
-  imports: [ProductosModule, OrdenesModule, NatsModule, AuthModule],
+  imports: [ProductosModule, OrdenesModule, NatsModule, AuthModule,HealthCheckModule],
   controllers: [],
   providers: [],
 })
